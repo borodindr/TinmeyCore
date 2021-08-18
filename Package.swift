914896dbeba7b8afd88b04dae59cd8843b99158a
Дмитрий Бородин <borodin.dr@pecom.ru>
2021-08-18 17:04:40 +0300

    Updated Package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TinmeyCore",
+    name: "tinmey-core",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
