e4e969c8661629d8bcbc524fbd3dc6ea76e8cd4f
Dmitry Borodin <borodindr@gmail.com>
2021-08-27 15:55:30 +0300

    Added WorkAPIModel

---
+++ Sources/TinmeyCore/APIModels/WorkAPIModel.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dmitry Borodin on 27.08.2021.
+//
+
+import Foundation
+
+public struct WorkAPIModel: Codable {
+    public let id: UUID?
+    public let createdAt: Date?
+    public let updatedAt: Date?
+    public let title: String
+    public let shortDescription: String
+    public let description: String
+    // layout
+    // tags
+    // comments
+}
+
+extension WorkAPIModel {
+    public struct Create: Codable {
+        public let title: String
+        public let shortDescription: String
+        public let description: String
+    }
+}
+
+extension WorkAPIModel {
+    public struct Short: Codable {
+        public let id: UUID?
+        public let createdAt: Date?
+        public let updatedAt: Date?
+        public let title: String
+        public let shortDescription: String
+    }
+}
