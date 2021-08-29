2c5685dc34cca3bd6f019c0090a30640fd44fce7
Dmitry Borodin <borodindr@gmail.com>
2021-08-29 17:19:20 +0300

    Added public init to WorkImageAPIModel

--- Sources/TinmeyCore/APIModels/WorkImageAPIModel.swift
+++ Sources/TinmeyCore/APIModels/WorkImageAPIModel.swift
@@ -11,4 +11,10 @@ public struct WorkImageAPIModel: Codable, Hashable {
     public let id: UUID
     public let downloadURL: String
     public let sortIndex: Int
+    
+    public init(id: UUID, downloadURL: String, sortIndex: Int) {
+        self.id = id
+        self.downloadURL = downloadURL
+        self.sortIndex = sortIndex
+    }
 }
