628244cbcbc556fe472d8dcc18c70bbcd50484d3
Dmitry Borodin <borodindr@gmail.com>
2021-10-20 19:28:31 +0300

    Added section subtitle

--- Sources/TinmeyCore/APIModels/Section/SectionAPIModel.swift
+++ Sources/TinmeyCore/APIModels/Section/SectionAPIModel.swift
@@ -9,10 +9,20 @@ import Foundation
 
 public struct SectionAPIModel: Codable, Hashable {
     public let type: SectionType
+    public let subtitle: String
     public let preview: Preview
     
-    public init(type: SectionAPIModel.SectionType, preview: SectionAPIModel.Preview) {
+    public var title: String {
+        preview.title
+    }
+    
+    public init(
+        type: SectionAPIModel.SectionType,
+        subtitle: String,
+        preview: SectionAPIModel.Preview
+    ) {
         self.type = type
+        self.subtitle = subtitle
         self.preview = preview
     }
 }
