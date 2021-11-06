3a1df493f2a4b2b3b74799ba26845e88aef0aa36
Dmitry Borodin <borodindr@gmail.com>
2021-11-05 22:20:53 -0500

    Deleted currentStatus from Profile

--- Sources/TinmeyCore/APIModels/ProfileAPIModel.swift
+++ Sources/TinmeyCore/APIModels/ProfileAPIModel.swift
@@ -10,14 +10,17 @@ import Foundation
 public struct ProfileAPIModel: Codable, Hashable {
     public let name: String
     public let email: String
-    public let currentStatus: String
     public let shortAbout: String
     public let about: String
     
-    public init(name: String, email: String, currentStatus: String, shortAbout: String, about: String) {
+    public init(
+        name: String,
+        email: String,
+        shortAbout: String,
+        about: String
+    ) {
         self.name = name
         self.email = email
-        self.currentStatus = currentStatus
         self.shortAbout = shortAbout
         self.about = about
     }
