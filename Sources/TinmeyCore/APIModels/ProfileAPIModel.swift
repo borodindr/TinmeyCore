75374248fc483358c81a1490fd14d34d30a6dd50
Dmitry Borodin <borodindr@gmail.com>
2021-12-13 17:57:22 -0600

    Added location to Profile

--- Sources/TinmeyCore/APIModels/ProfileAPIModel.swift
+++ Sources/TinmeyCore/APIModels/ProfileAPIModel.swift
@@ -10,17 +10,20 @@ import Foundation
 public struct ProfileAPIModel: Codable, Hashable {
     public let name: String
     public let email: String
+    public let location: String
     public let shortAbout: String
     public let about: String
     
     public init(
         name: String,
         email: String,
+        location: String,
         shortAbout: String,
         about: String
     ) {
         self.name = name
         self.email = email
+        self.location = location
         self.shortAbout = shortAbout
         self.about = about
     }
