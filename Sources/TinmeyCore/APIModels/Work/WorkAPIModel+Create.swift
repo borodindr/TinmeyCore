ce30f73069ff96d80baee601689dfc6239ec3f12
Dmitry Borodin <borodindr@gmail.com>
2022-03-13 14:11:07 -0500

    Updated WorkAPIModel

--- Sources/TinmeyCore/APIModels/Work/WorkAPIModel+Create.swift
+++ Sources/TinmeyCore/APIModels/Work/WorkAPIModel+Create.swift
@@ -12,23 +12,17 @@ extension WorkAPIModel {
         public let title: String
         public let description: String
         public let tags: [String]
-        public let seeMoreLink: URL?
-        public let bodyIndex: Int
         public let images: [Image.Create]
         
         public init(
             title: String,
             description: String,
             tags: [String] = [],
-            seeMoreLink: URL?,
-            bodyIndex: Int,
             images: [Image.Create]
         ) {
             self.title = title
             self.description = description
             self.tags = tags
-            self.seeMoreLink = seeMoreLink
-            self.bodyIndex = bodyIndex
             self.images = images
         }
     }
--- Sources/TinmeyCore/APIModels/Work/WorkAPIModel.swift
+++ Sources/TinmeyCore/APIModels/Work/WorkAPIModel.swift
@@ -14,8 +14,6 @@ public struct WorkAPIModel: Codable, Hashable {
     public let title: String
     public let description: String
     public let tags: [String]
-    public let seeMoreLink: URL?
-    public let bodyIndex: Int
     public let images: [Image]
     
     public init(
@@ -25,8 +23,6 @@ public struct WorkAPIModel: Codable, Hashable {
         title: String,
         description: String,
         tags: [String],
-        seeMoreLink: URL?,
-        bodyIndex: Int,
         images: [Image]
     ) {
         self.id = id
@@ -35,8 +31,6 @@ public struct WorkAPIModel: Codable, Hashable {
         self.title = title
         self.description = description
         self.tags = tags
-        self.seeMoreLink = seeMoreLink
-        self.bodyIndex = bodyIndex
         self.images = images
     }
 }
